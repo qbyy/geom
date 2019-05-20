c612e717fbd38a2a4e9116f6a6577f25edafe0b0
Ковалик Руслан Геннадьевич <ip814s11@mwp08.csc.local>
2019-05-20 14:25:20 +0700

    Eddit from travis # Пожалуйста, введите сообщение коммита для ваших изменений. Строки,

--- src/func.c
+++ src/func.c
@@ -10,4 +10,3 @@ void Math(float x1, float y1, float r1, float x2, float y2, float r2);
     r = r1 + r2;
     peresechenie = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 }
-
--- src/func.h
+++ src/func.h
@@ -1,6 +1,6 @@
-#ifndef H_FUNC	
+#ifndef H_FUNC
 #define H_FUNC
 
-void Math (float x1, float y1, float r1, float x2, float y2, float r2);
+void Math(float x1, float y1, float r1, float x2, float y2, float r2);
 
 #endif
